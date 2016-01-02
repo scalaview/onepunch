5f5ed39130cd45b932b108a0dc5f2d5b1532be7d
benko <chailink100@gmail.com>
2016-01-02 22:58:47 +0800

    finish wechat payment

--- app.js
+++ app.js
@@ -177,6 +177,5 @@ app.use(function(err, req, res, next){
 var server = app.listen(app.get('port'), function () {
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
 });
