0e7652e36c154f2eab6f981cce4c9367106081a6
benko <chailink100@gmail.com>
2016-01-23 23:21:31 +0800

    edit name

--- helpers/index.js
+++ helpers/index.js
@@ -269,7 +269,7 @@ function discount(customer, trafficPlan){
     discount = customer.level.discount
   }
 
-  if(discount >= (config.discountmin || 0.60) ){
+  if(discount >= (config.mindiscount || 0.60) ){
     return (trafficPlan.cost * discount).toFixed(2)
   }else{
     return trafficPlan.cost
