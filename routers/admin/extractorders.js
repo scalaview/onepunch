e0feac7c3332da225ba53f0b522425897f94ef1a
scalaview <chailink100@gmail.com>
2016-04-07 14:42:04 +0800

    fix refund id

--- routers/admin/extractorders.js
+++ routers/admin/extractorders.js
@@ -255,7 +255,7 @@ admin.post("/extractorder/:id/refund", function(req, res){
       var payment = helpers.payment,
           total_amount = Math.round(extractOrder.total * 100).toFixed(0),
           refund = {
-            out_trade_no: config.token + "_" + extractOrder.phone + "_" + extractOrder.id,
+            out_trade_no: config.token + "_" + extractOrder.phone + "_" + extractOrder.id + "_" + total_amount,
             out_refund_no: "refund_" + config.token + "_" + extractOrder.phone + "_" + extractOrder.id + "_" + total_amount,
             total_fee: total_amount,
             refund_fee: total_amount
